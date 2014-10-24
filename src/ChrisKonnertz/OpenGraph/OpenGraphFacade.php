5a74771bdcaad10c86e4af2d048e4ad894c853e6
siconize <siconize@gmail.com>
2014-10-24 22:45:04 +0200

    Added Laravel service provider and facade

---
+++ src/ChrisKonnertz/OpenGraph/OpenGraphFacade.php
@@ -0,0 +1,14 @@
+<?php namespace ChrisKonnertz\OpenGraph;
+
+use Illuminate\Support\Facades\Facade;
+
+class OpenGraphFacade extends Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'opengraph'; }
+
+}
---
+++ src/ChrisKonnertz/OpenGraph/OpenGraphServiceProvider.php
@@ -0,0 +1,16 @@
+<?php namespace ChrisKonnertz\OpenGraph;
+
+use ChrisKonnertz\OpenGraph\OpenGraph;
+use Illuminate\Support\ServiceProvider;
+
+class OpenGraphServiceProvider extends ServiceProvider {
+
+    public function register()
+    {
+        $this->app->bind('opengraph', function()
+        {
+            return new OpenGraph;
+        });
+    }
+
+}
