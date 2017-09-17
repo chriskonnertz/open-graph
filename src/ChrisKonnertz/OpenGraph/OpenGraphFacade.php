126b5552b81f550b78cc67f0b390555a50f12f35
Chris Konnertz <siconize@gmail.com>
2017-09-17 11:04:29 +0200

    Corrected formatting

--- src/ChrisKonnertz/OpenGraph/OpenGraphFacade.php
+++ src/ChrisKonnertz/OpenGraph/OpenGraphFacade.php
@@ -1,14 +1,19 @@
-<?php namespace ChrisKonnertz\OpenGraph;
+<?php 
+
+namespace ChrisKonnertz\OpenGraph;
 
 use Illuminate\Support\Facades\Facade;
 
-class OpenGraphFacade extends Facade {
+class OpenGraphFacade extends Facade 
+{
 
     /**
      * Get the registered name of the component.
      *
      * @return string
      */
-    protected static function getFacadeAccessor() { return 'opengraph'; }
+    protected static function getFacadeAccessor() { 
+        return 'opengraph'; 
+    }
 
 }
